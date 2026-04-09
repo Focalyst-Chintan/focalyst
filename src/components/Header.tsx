5f26d394830453662e9500ffb89e2be0dfd3daa1
Sohan Tamang <sohantamang@Sohans-MacBook-Air.local>
2026-04-09 13:08:28 +0530

    chore(ui): simplify header branding to text wordmark only

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
 import { createClient } from '@/lib/supabase'
 import { UserAccountIcon, AIChatIcon } from '@/components/icons'
 import { useChat } from '@/context/ChatContext'
